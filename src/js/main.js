10916a6d4c56915e4c5b9e0982ce74c07fde3412
kingminor <michaelgbrightman@yahoo.com>
2025-09-25 11:06:47 -0600

    Update hero banner and header styles, enhance JS content
    Refactored CSS for .global-header to use flexbox and improved accessibility. Added new styles for .hero-banner and .hero-banner-subcontent. Updated index.html to add id to hero image. Enhanced main.js to dynamically populate disclaimer, document title, hero image, and hero banner subcontent using park data.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,23 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+document.title = parkData.name;
+
+const heroImage = document.querySelector("#hero-image");
+heroImage.src = parkData.images[0].url;
+
+const heroBannerSubcontent = document.querySelector(".hero-banner-subcontent");
+heroBannerSubcontent.innerHTML = parkInfoTemplate(parkData);
+
+function parkInfoTemplate(info) {
+  return `<a href="/" class="hero-banner__title">${info.name}</a>
+  <p class="hero-banner__subtitle">
+    <span>${info.designation}</span>
+    <span>${info.states}</span>
+  </p>`;
+}
